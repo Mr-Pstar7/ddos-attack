50c2a3afee07e7d831ab2c38218a4224388684a3
Mr-Pstar7 <165870046+Mr-Pstar7@users.noreply.github.com>
2024-04-18 16:11:59 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Sedang menginstal module..."
+apt update && apt upgrade 
+apt install nodejs
+apt install python
+pip install bs4
+pip install mechanize
+pip3 install colorama
+apt install wget
+pip3 install wget
+pip3 install requests
+pip3 install sockets
+pip3 install os-sys
+pip3 install times
+pip3 install sys
+pip3 install ipaddress
+pip3 install beautifulsoup4
+pip3 install argparse
+pip3 install random2
+pip3 install scapy
+pip install pycryptodome
+npm i
